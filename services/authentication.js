bf5f81f8e3752355c34db179d5592491e0070242
Johannes Kasser <johannes.kasser@stud.fh-campuswien.ac.at>
2023-03-11 13:57:54 +0100

    Add Process Variables dor local runng version

--- services/authentication.js
+++ services/authentication.js
@@ -51,7 +51,8 @@ module.exports = {
         res.cookie('jwt', token, {
             httpOnly: true,
             domain: process.env.Domain,
-            path: '/dashboard',
+            path: '/',
+            sameSite: "none",
             maxAge: 24 * 60 * 60 * 1000 //1 Day
         })
 
